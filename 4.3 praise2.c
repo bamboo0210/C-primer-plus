f34c709771c9b444ea892484c0f4de3ab0a5fb02
ohmyhair55 <108349409+ohmyhair55@users.noreply.github.com>
2022-07-22 22:02:45 +0800

    Add files via upload

---
+++ 4.3 praise2.c
@@ -0,0 +1,16 @@
+//praise2.c
+//如果编译器不识别%zd,尝试换成%u或%lu。
+#include<stdio.h>
+#include<string.h>//提供strlen()函数的原型
+#define PRAISE "You are an extraordinary being."
+int main(void)
+{
+	char name[40] = { 0 };
+	printf("What's your name?");
+	scanf_s("%s", name, 40);
+	printf("Hello,%s.%s\n", name, PRAISE);
+	printf("Your name of %zd letters occupies %zd memory cell.\n", strlen(name), sizeof(name));
+	printf("The phrase of praise has %zd letters", strlen(PRAISE));
+	printf("and occupies %zd memory cells.\n", sizeof PRAISE);
+	return 0;
+}
